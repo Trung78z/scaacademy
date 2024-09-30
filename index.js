1b84a86a1f9c1a2109aed94a2d0df1efff6ff26b
Trung78z <trungpspy@gmail.com>
2024-09-30 21:10:22 +0700

    .

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public/mobile/index.html"));
   } else {
     app.use(express.static(path.join(__dirname, "public/desktop")));
+    app.use(express.static(path.join(__dirname, "public")));
     res.sendFile(path.join(__dirname, "public/desktop/index.html"));
   }
 });
